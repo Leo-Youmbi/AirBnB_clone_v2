f52910786eab96d498ea602e24c1e1f72ce5240a
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-07 20:42:44 +0100

    feat: AirBnB_clone_v2 mandatory tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,13 @@ import time
 
 
 def do_pack():
+    """
+    Create a compressed tarball archive of the web_static/ directory.
+
+    Returns:
+        str: Path to the created tarball archive.
+            If an exception occurs during the process, returns None.
+    """
     time_string = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
