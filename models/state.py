841a34566f374034684b11c24217ebc02a8a8b76
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-21 12:19:32 +0100

    feat: AirBnB clone - MySQL tasks

--- models/state.py
+++ models/state.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship, backref
+from models import storage_type
 from models.city import City
-from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-    
+    __tablename__ = 'states'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
-
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
         @property
         def cities(self):
-            """ Returns the list of City
-            """
-            the_cities = []
-            for city in models.storage.all(City).values():
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
                 if city.state_id == self.id:
-                    the_cities.append(city)
-            return the_cities
+                    related_cities.append(city)
+            return related_cities
