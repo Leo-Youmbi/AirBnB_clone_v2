b5792b929fdded486dcf28f322c77136b5973a51
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-23 14:12:41 +0100

    feat: Web Framework mandatory task

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all(State)
+    for state in states.values():
+        print(f"name: {state.name}, id: {state.id}")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
